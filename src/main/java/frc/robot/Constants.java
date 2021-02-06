7b8659fa449edb8703310ff12ac13a3ff5ad1931
Koopa <codingkoopa@gmail.com>
2021-02-06 14:59:08 -0500

    Add code licensing.
    This commit adds the 3-clause BSD license, and adds a copyright notice
    on behalf of Team 564 to each source file.
    
    For more information on the BSD licenses, see:
    https://en.wikipedia.org/wiki/BSD_licenses

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,3 +1,7 @@
+// Copyright (c) Team 564.
+// Open Source Software; you can modify and/or share it under the terms of
+// the BSD license file in the root directory of this project.
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
