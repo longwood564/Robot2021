ffdb6018a247c2c5518c66fc0aed8f59824e854e
Koopa <codingkoopa@gmail.com>
2021-02-09 20:59:30 -0500

    Start new Constants class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,46 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+// TODO: Move everything inside here.
+public final class Constants {
+  public static final class RoboRIO {
+    public static final class CAN {
+      /** The port of the front left drive motor. */
+      public static final int kPortMotorDriveFrontLeft = 1;
+      /** The port of the back left drive motor. */
+      public static final int kPortMotorDriveBackLeft = 2;
+      /** The port of the front right drive motor. */
+      public static final int kPortMotorDriveFrontRight = 3;
+      /** The port of the back right drive motor. */
+      public static final int kPortMotorDriveBackRight = 4;
+      /** The port of the intake motor. */
+      public static final int kPortMotorIntake = 7;
+      /** The port of the left launcher motor. */
+      public static final int kPortMotorLauncherLeft = 8;
+      /** The port of the conveyor belt. */
+      public static final int kPortMotorBelt = 10;
+      /**
+       * The port of the right launcher motor. On the electronics board, this is #8b, but this
+       * couldn't be replicated due to software limitaitons.
+       */
+      public static final int kPortMotorLauncherRight = 11;
+    }
+
+    public static final class DIO {
+      /** The ports of the left drive encoder. */
+      public static final int[] kPortsEncoderDriveLeft = {0, 1, 2, 3};
+      /** The ports of the right drive encoder. */
+      public static final int[] kPortsEncoderDriveRight = {4, 5, 6, 7};
+    }
+  }
+
+  public static final class DrivetrainConstants {
+    // TODO: Check these speeds - especially kSpeedFast.
+    /** The slow speed to drive at. */
+    public static final double kSpeedSlow = 0.5;
+    /** The normal speed to drive at. */
+    public static final double kSpeedNormal = 0.85;
+    /** The fast speed to drive at. */
+    public static final double kSpeedFast = 1;
+  }
+}
