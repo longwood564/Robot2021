f4aa50e006f85282037510ef7e68f6b42639f7f5
Koopa <codingkoopa@gmail.com>
2021-02-12 15:44:22 -0500

    Remove second camera view.
    I confirmed on the FRC Discord that data will not be shared between the
    two views.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -18,8 +18,6 @@ import io.github.oblarg.oblog.annotations.Log;
  * with coprocessors to provide object tracking data from the other camera.
  */
 public class VisionSubsystem extends SubsystemBase implements Loggable {
-  // TODO: Check whether adding this twice consumes twice the bandwidth.
-  @Log.CameraStream(name = "Camera", width = 7, height = 6, rowIndex = 0, columnIndex = 0)
   @Log.CameraStream(
       name = "Camera",
       width = 7,
